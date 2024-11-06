563d9ab5f02e03b771da21557d882d9682ed9c57
Muhammed Mehti GÜLER <mehti.guler@gmail.com>
2024-11-06 11:45:33 +0300

    Shared Project Added

--- Catalog.Api/Program.cs
+++ Catalog.Api/Program.cs
@@ -1,14 +1,13 @@
-
-
 using Catalog.Api.Options;
+using Catalog.Api.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 builder.Services.AddOptionsExt();
+builder.Services.AddDatabaseServiceExt();
+
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
---
+++ Catalog.Api/Repositories/RepositoryExt.cs
@@ -0,0 +1,26 @@
+﻿using Catalog.Api.Options;
+using MongoDB.Driver;
+
+namespace Catalog.Api.Repositories
+{
+    public static class RepositoryExt
+    {
+        public static IServiceCollection AddDatabaseServiceExt(this IServiceCollection services)
+        {
+            services.AddSingleton<IMongoClient, MongoClient>(sp =>
+            {
+                var options = sp.GetRequiredService<MongoOptions>();
+                return new MongoClient(options.ConnectionString);
+            });
+
+            services.AddScoped(sp =>
+            {
+                var mongoClient = sp.GetRequiredService<IMongoClient>();
+                var options = sp.GetRequiredService<MongoOptions>();
+
+                return AppDbContext.Create(mongoClient.GetDatabase(options.DatabaseName));
+            });
+            return services;
+        }
+    }
+}
